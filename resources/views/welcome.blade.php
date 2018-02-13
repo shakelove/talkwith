126884d75da9b68b7b0d1a3f3d7bd10df4930a97
Taro Kanno <taro1225.enjoy@icloud.com>
2018-02-13 05:36:03 +0000

    user login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,16 @@
 @extends('layouts.app')
 
 @section('cover')
+@if (Auth::check())
+    <div class="cover">
+        <div class="cover-inner">
+            <div class="cover-contents">
+                <h1>英語で話す</h1>
+                <a href="" class="btn btn-success btn-lg">検索</a>
+            </div>
+        </div>
+    </div>
+@else
     <div class="cover">
         <div class="cover-inner">
             <div class="cover-contents">
@@ -9,6 +19,7 @@
             </div>
         </div>
     </div>
+@endif
 @endsection
 
 @section('content')
