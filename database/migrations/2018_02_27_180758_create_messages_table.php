9b10a119f58858b96dc678cdd823b90f263cb2fc
Taro Kanno <taro1225.enjoy@icloud.com>
2018-02-27 09:15:02 +0000

    delete migrations

--- database/migrations/2018_02_27_180758_create_messages_table.php
+++ database/migrations/2018_02_27_180758_create_messages_table.php
@@ -14,11 +14,12 @@ class CreateMessagesTable extends Migration
     {
         Schema::create('messages', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('content');  
+            $table->$table->string('content');
             $table->integer('from_id')->unsigned()->index();
             $table->integer('to_id')->unsigned()->index();
             $table->timestamps();
             
+            // 外部キー設定
             $table->foreign('from_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('to_id')->references('id')->on('users')->onDelete('cascade');
         });
